fb76f8d59d5934ccc6c7acd0514dfde9a71eea80
Mohamed Sharif <mohamedshariif690@gmail.com>
2025-09-14 17:23:48 +0300

    warning

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require ("express")
 const mongoose = require ("mongoose")
 const cors = require("cors")
+const ProductRouter=require("./Router/product")
 
 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(ProductRouter)
+app.use("/allImages", express.static("images"))
 
 
 mongoose.connect("mongodb://localhost:27017/supermarket").then(()=> console.log("sucesseful"))
